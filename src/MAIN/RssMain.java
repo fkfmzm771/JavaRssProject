a46141e13d24b53d2f6495b029909118d1c80820
DESKTOP-PSB5I22\fkfmz <fkfmzm77@naver.com>
2018-11-29 01:50:05 +0900

    링크 연결 성공

--- src/MAIN/RssMain.java
+++ src/MAIN/RssMain.java
@@ -1,7 +1,16 @@
 package MAIN;
 
-public class RssMain {
-    public static void main(String[] args) {
+import Manager.NyaaFeedParser;
+import VO.Nyaa_si_Feed;
+import VO.Nyaa_si_FeedMessage;
 
-    }
+public class RssMain {
+	public static void main(String[] args) {
+
+		NyaaFeedParser parser = new NyaaFeedParser("https://nyaa.si/?page=rss&q=baki&c=0_4&f=0");
+		Nyaa_si_Feed feed = parser.readFeed();
+		for (Nyaa_si_FeedMessage message : feed.getMessages()) {
+			System.out.println(message);
+		}
+	}
 }
