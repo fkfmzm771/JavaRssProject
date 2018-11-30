74e5843215ee5c6e5cef6372390b8b295d7a8e96
hyunungLim <fkfmzm77@naver.com>
2018-11-30 19:52:01 +0900

    스트림 연결 오류

--- src/MAIN/RssMain.java
+++ src/MAIN/RssMain.java
@@ -2,6 +2,7 @@ package MAIN;
 
 import Manager.NyaaFeedParser;
 import Manager.TokyoFeedParser;
+import VO.FileData;
 import VO.Nyaa_si.Nyaa_si_Feed;
 import VO.Nyaa_si.Nyaa_si_FeedMessage;
 
@@ -9,11 +10,13 @@ public class RssMain {
 	public static void main(String[] args) {
 
 		TokyoFeedParser parser1 = new TokyoFeedParser("https://www.tokyotosho.info/rss.php?terms=baki");
-		NyaaFeedParser parser = new NyaaFeedParser("http://leopard-raws.org/rss.php?terms=piece");
+		NyaaFeedParser parser = new NyaaFeedParser("https://nyaa.si/?page=rss&q=baki&c=0_0&f=0");
 		Nyaa_si_Feed feed = parser.readFeed();
 		for (Nyaa_si_FeedMessage message : feed.getMessages()) {
+			parser.saveUrlList(new FileData(message.getTitle(),message.getLink()));
 			System.out.println(message);
 		}
+		parser.saveFile();
 
 	}
 }
